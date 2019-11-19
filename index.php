7b9fb02138ebfb2e08ef50cc84b3da1850d1f88f
Victor Pinheiro <victor.info.89@gmail.com>
2019-11-19 15:54:39 -0300

    Listagem de Cidades.

--- index.php
+++ index.php
@@ -4,4 +4,20 @@ echo "<h2 style='color: blue;'>Sexto Build!</h2>";
 $host= gethostname();
 $ip = gethostbyname($host);                    
 echo $ip;
+
+echo "<br><hr>";
+echo "<h2>Cidades cadastradas no Banco de Dados:</h2>";
+$conn = new mysqli("mysql", "redhat", "redhat@123", "workshop");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$result = $conn->query("SELECT nome FROM cidade");
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "<h3>" . $row["nome"] . "</h3>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
 ?>
