ae650f83fedd52b0911284d960740b4d3fbb4197
Victor Pinheiro <victor.info.89@gmail.com>
2019-11-19 15:26:22 -0300

    Terceiro commit!

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
 echo "<h1>Openshift Workshop v1.0</h1> ";
 echo "<h2 style='color: blue;'>Segundo Build!</h2>";
+$host= gethostname();
+$ip = gethostbyname($host);                    
+echo $ip;
 ?>
