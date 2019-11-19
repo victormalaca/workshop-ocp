deec8e3bb63f265fb4aaec33c3a2e3785c149356
Victor Pinheiro <victor.info.89@gmail.com>
2019-11-19 15:44:45 -0300

    Sexto commit!

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-echo "<h1>Openshift Workshop v1.0</h1> ";
-echo "<h2 style='color: blue;'>Quinto Build!</h2>";
+echo "<h1>Openshift Workshop v6.0</h1> ";
+echo "<h2 style='color: blue;'>Sexto Build!</h2>";
 $host= gethostname();
 $ip = gethostbyname($host);                    
 echo $ip;
