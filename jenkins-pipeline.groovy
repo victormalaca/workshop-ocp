c8bea071ac0a3896488fdf06362f6eb37230d605
Victor Pinheiro <victor.info.89@gmail.com>
2019-11-20 10:13:15 -0300

    Update jenkins-pipeline.groovy

--- jenkins-pipeline.groovy
+++ jenkins-pipeline.groovy
@@ -1,14 +1,14 @@
 node('maven') {
     stage('build') {
         echo 'building app :)'
-        openshiftBuild(buildConfig: 'workshop-ocp', showBuildLogs: 'true')
+        openshiftBuild(buildConfig: 'workshop-ocp2', showBuildLogs: 'true')
     }
     stage('verify') {
         echo 'dummy verification....'
     }
     stage('deploy') {
         input 'Manual Approval'
-        openshiftDeploy(deploymentConfig: 'workshop-ocp')
+        openshiftDeploy(deploymentConfig: 'workshop-ocp2')
     }
     stage('promoting to QA') {
        echo 'fake stage...'
